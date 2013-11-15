4ffc698e64385e6948a4829b1f51f9def0b04f69
Mike Harris <mharris717@gmail.com>
2013-11-15 11:01:01 -0500

    use local tmp

--- lib/overapp/util/tmp_dir.rb
+++ lib/overapp/util/tmp_dir.rb
@@ -1,26 +1,31 @@
 module Overapp
   class TmpDir
-    def self.with(ops={})
-      dir = "/tmp/#{rand(1000000000000000000)}_overapp"
-      `mkdir #{dir}`
-      if block_given?
-        Dir.chdir(dir) do
+    class << self
+      def base_dir
+        File.expand_path(File.dirname(__FILE__) + "/../../../tmp")
+      end
+      def with(ops={})
+        dir = "#{base_dir}/#{rand(1000000000000000000)}"
+        `mkdir #{dir}`
+        if block_given?
+          Dir.chdir(dir) do
+            yield dir
+          end
+        else
+          dir
+        end
+      ensure
+        if block_given?
+          ec "rm -rf #{dir}", :silent => true
+        end
+      end
+
+      def with_repo_path(url)
+        url = Overapp.to_proper_dir(url)
+        with do |dir|
+          `git clone #{url} . 2>&1`
           yield dir
         end
-      else
-        dir
-      end
-    ensure
-      if block_given?
-        ec "rm -rf #{dir}", :silent => true
-      end
-    end
-
-    def self.with_repo_path(url)
-      url = Overapp.to_proper_dir(url)
-      with do |dir|
-        `git clone #{url} . 2>&1`
-        yield dir
       end
     end
   end
